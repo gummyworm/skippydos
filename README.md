737bc781357944430e05af7c5e832266de3a9b4c
Bryce <brycemwilson@gmail.com>
2015-11-22 19:09:38 -0600

    Add the KERNAL

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # skippydos
 JiffyDOS KERNAL clone for the Vic 20. Contains some extra features like autostart-disable and RAM block selection.
+
+This project was developed from June 6th 2011 to September 21st 2012. 
+The only remaining bug I'm aware of is that <CTRL-S> does not pause a BASIC or directory listing.
+
+
---
+++ test.sh
@@ -0,0 +1,13 @@
+# compile and test jiffy replacement.
+# use the command line argument "NTSC" to test on an NTSC vic-20
+# otherwise this code will run on an emulated PAL machine
+cl65 -t none --start-addr 0xe000 vic20kernal.asm -o jiffy.bin
+read
+if [ $1 "ntsc" ]
+then
+    xvic -default -ntsc -memory all -kernal jiffy.bin -dos1541 JiffyDOS_C1541.bin -truedrive -8 TGA_v7.d64
+        
+else
+    xvic -default -pal -memory all -kernal jiffy.bin -dos1541 JiffyDOS_C1541.bin -truedrive -8 TGA_v7.d64
+fi
+
